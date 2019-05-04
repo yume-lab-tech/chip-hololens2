a2091aa9015ae5b1c2372f56f63f8722b1b683c5
MasayukiTsuji <tu.kun.0220@gmail.com>
2019-05-05 00:20:57 +0900

    グローバルタップたいおう

--- Assets/Scripts/CustomGlobalHandler.cs
+++ Assets/Scripts/CustomGlobalHandler.cs
@@ -2,16 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
 
-public class CustomGlobalHandler : InputSystemGlobalListener
+[RequireComponent(typeof(GameObject))]
+public class CustomGlobalHandler : MonoBehaviour
 {
-    private void OnEnable()
-    {
-        Debug.Log("Enabled");
-    }
+    [SerializeField]
+    public GameObject holeMoneyObject;
 
-    private void OnDisable()
+    public void GenerateMoneyHole()
     {
-        Debug.Log("Disabled");
+        // 視線を取得する
+        IMixedRealityEyeGazeProvider gazeProvider = MixedRealityToolkit.InputSystem.EyeGazeProvider;
+        Vector3 gazeDirection = gazeProvider.GazeDirection;
+        Vector3 gazeOrigin = gazeProvider.GazeOrigin;
+
+        // Objectの生成
+        GameObject newObject = Instantiate(holeMoneyObject);
+
+        // カメラの向き先からMoneyHoleの向き先を決定する。
+        Camera camera = CameraCache.Main;
+        Quaternion holeRotation = new Quaternion(0.0f, camera.transform.localRotation.y, 0.0f, camera.transform.localRotation.w);
+
+        // 視線の先にあるオブジェクトを取得する。
+        RaycastHit hitInfo;
+        if (Physics.Raycast(gazeOrigin, gazeDirection.normalized, out hitInfo, 5f))
+        {
+            Debug.Log("RayHit!!!");
+
+            // 既に作成済みのオブジェクトを解放する。
+            GameObject hitObject = hitInfo.collider.gameObject;
+            if (hitObject.CompareTag(holeMoneyObject.tag))
+            {
+                Destroy(hitObject, 0.0f);
+                return;
+            }
+
+            // ヒットしたオブジェクトの座標にオブジェクトを配置する。
+            newObject.transform.localRotation = holeRotation;
+            newObject.transform.position = hitInfo.transform.position;
+        }
+        else
+        {
+            Debug.Log("Create new money hole");
+
+            // デフォルトの座標のオブジェクトを配置する。
+            Vector3 defaultPosition = gazeOrigin + gazeDirection.normalized * 2f;
+            newObject.transform.localRotation = holeRotation;
+            newObject.transform.position = defaultPosition;
+        }
+
+        // オブジェクトの有効化
+        newObject.SetActive(true);
     }
 }
